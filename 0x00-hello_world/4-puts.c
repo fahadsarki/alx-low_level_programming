1aebd54ddac4cc7a1834610ba423a5134371ea83
fahadsarki <fahads.mukhtar@gmail.com>
2022-09-08 09:30:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - Write '"Programming is like building a multilingual puzzle'
+* main - Write this '"Programming is like building a multilingual puzzle'
 * Return: Always 0 (success)
 */
 int main(void)
