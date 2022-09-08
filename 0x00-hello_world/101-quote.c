b5ad7bb3fff67b2c32cf68322f8ddf442ec300bb
fahadsarki <fahads.mukhtar@gmail.com>
2022-09-08 10:30:21 -0700

    standard error print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - entry point of program
+ * Return: 1 (standard error)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
