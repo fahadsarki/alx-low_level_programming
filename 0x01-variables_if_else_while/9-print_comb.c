8adae9e08886c60f4af351823fbc180c741cd2ae
fahadsarki <fahads.mukhtar@gmail.com>
2022-09-09 14:57:24 -0700

    ,number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - entry point
+* description: numbers with commas
+* Return: 0 man
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
