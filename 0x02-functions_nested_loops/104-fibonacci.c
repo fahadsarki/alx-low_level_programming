c8775e83c66fb9a54194111ca483aa596f4be0db
fahadsarki <fahads.mukhtar@gmail.com>
2022-09-13 16:35:24 -0700

    no long

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* main - Prints first 89 numbers of the fibonacci
+* Return: 0
+*/
+int main(void)
+{
+	int i, l;
+	unsigned int a, b, c;
+
+	a = 1;
+	b = 2;
+	l = 89;
+
+	for (i = 0; i < l; i++)
+	{
+		if (i == 87)
+		{
+			_putchar(a);
+		} else
+		{
+			_putchar(a);
+			_putchar(',');
+			_putchar(' ');
+			c = a + b;
+			a = b;
+			b = c;
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
