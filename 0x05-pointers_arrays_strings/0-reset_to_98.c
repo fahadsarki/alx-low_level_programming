6c9ec39cd80fab2da46d2cd5fad48e32a992c175
fahadsarki <fahads.mukhtar@gmail.com>
2022-09-19 06:34:33 -0700

    pointer value update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * @n: variable pointing to integer
+ * Return - always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
