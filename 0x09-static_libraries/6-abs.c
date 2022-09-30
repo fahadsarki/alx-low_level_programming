ceb677f938c65f8fd9528a3f8eee07a86b0b638e
fahadsarki <fahads.mukhtar@gmail.com>
2022-09-30 15:33:13 -0700

    “mandatory”

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - print absolute value of integer
+ * @c: integer to be computed
+ * Return: Absolute value
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (c * -1);
+}
