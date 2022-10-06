4f101b61b5420ba09ea567e5e92e42df385c449a
fahadsarki <fahads.mukhtar@gmail.com>
2022-10-06 15:03:13 -0700

    “mandatory”

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,26 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _calloc -allocated memoria for nmeb elemn de zise bytes
- * @nmemb: number of element in the array
- * @size: bytes for each position in array
- * Return: pointer void
+ * _calloc - A function that allocates memory to an array using malloc
+ * @nmemb: number of memory spaces
+ * @size: size of nmemb
+ * Return: returns pointer to allocated space, or NULL if it fails
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *fahad;
-	unsigned int i;
+	unsigned int i = 0;
+	char *space;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	fahad = malloc(nmemb * size);
-	if (fahad == NULL)
+	space = malloc(nmemb * size);
+	if (space == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb * size; i++)
-		fahad[i] = 0;
+	for (; i < nmemb * size; i++)
+		*(space + i) = 0;
 
-	return (fahad);
-
+	return (space);
 }
