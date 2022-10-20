ecc58ed1558efc0f3c5566b967f50602ac321da9
fahadsarki <fahads.mukhtar@gmail.com>
2022-10-20 15:17:06 -0700

    “mandatory”

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,18 +1,17 @@
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
+#ifndef SINGLY_LINKED_LIST_H_
+#define SINGLY_LINKED_LIST_H_
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 
 /**
- *struct list_s - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  * Description: singly linked list node structure
- *for Holberton project
- */
+ * for ALX project
+*/
+
 typedef struct list_s
 {
 	char *str;
@@ -20,6 +19,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
